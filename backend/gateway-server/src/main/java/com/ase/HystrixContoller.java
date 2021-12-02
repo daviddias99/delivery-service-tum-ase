1120c21d17eb52f0e8ed8d450193ecff04053670
erengulum <b21627265@cs.hacettepe.edu.tr>
2021-12-02 01:22:45 +0100

    API Gateway is implemented Hystrix Dashboard is implemented Account Service is converted into User Service

---
+++ backend/gateway-server/src/main/java/com/ase/HystrixContoller.java
@@ -0,0 +1,27 @@
+package com.ase;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("fallbacks")
+public class HystrixContoller {
+
+
+    @GetMapping("/user")
+    public String userFallback(){
+        return "User Service is not available";
+    }
+
+    @GetMapping("/box")
+    public String boxFallback(){
+        return "Box Service is not available";
+    }
+
+    @GetMapping("/delivery")
+    public String deliveryFallback(){
+        return "Delivery Service is not available";
+    }
+}
