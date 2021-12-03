a8a03de2b8a08a1d47814d1743eaf753c783dd4f
daviddias99 <up201705373@fe.up.pt>
2021-12-03 19:43:45 +0100

    Homepage starter

--- frontend/src/components/common/Layout/Layout.tsx
+++ frontend/src/components/common/Layout/Layout.tsx
@@ -1,17 +1,14 @@
-import React, { useEffect, useState, FunctionComponent } from 'react';
-import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
+import React, { useState } from 'react';
+import { styled } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
-import Badge from '@mui/material/Badge';
 import MenuIcon from '@mui/icons-material/Menu';
-import NotificationsIcon from '@mui/icons-material/Notifications';
 import Sidebar from './Sidebar/Sidebar';
-
-import api from 'services/api';
+import AppLogo from 'components/common/Layout/AppLogo/AppLogo'; // Import using relative path
+import Image from 'assets/images/bg-blured.jpg'; // Import using relative path
 
 const drawerWidth: number = 240;
 
@@ -21,13 +18,15 @@ interface AppBarProps extends MuiAppBarProps {
 
 const AppBar = styled(MuiAppBar, {
   shouldForwardProp: (prop) => prop !== 'open',
-})<AppBarProps>(({ theme, open }) => ({
-  zIndex: theme.zIndex.drawer + 1,
+})<AppBarProps & { hassidebar: boolean }>(({ theme, open, hassidebar }) => ({
+  zIndex: hassidebar ? theme.zIndex.drawer + 1 : 0,
   transition: theme.transitions.create(['width', 'margin'], {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
+  },
+  ),
+  backgroundColor: 'primary',
+  ...(open && hassidebar && {
     marginLeft: drawerWidth,
     width: `calc(100% - ${drawerWidth}px)`,
     transition: theme.transitions.create(['width', 'margin'], {
@@ -37,30 +36,27 @@ const AppBar = styled(MuiAppBar, {
   }),
 }));
 
-const mdTheme = createTheme();
+type LayoutProps = {
+  hasSidebar: boolean
+  children: React.ReactNode
+}
 
-const Layout: FunctionComponent = ({ children }) => {
+const Layout = ({ hasSidebar, children }: LayoutProps) => {
   const [open, setOpen] = useState(true);
   const toggleDrawer = () => {
     setOpen(!open);
   };
 
-  useEffect(
-    () => {
-      api.login({ username: 'user' }, (a) => console.log(a));
-    }
-  );
-
   return (
-    <ThemeProvider theme={mdTheme}>
-      <Box sx={{ display: 'flex' }}>
-        <CssBaseline />
-        <AppBar position="absolute" open={open}>
-          <Toolbar
-            sx={{
-              pr: '24px', // keep right padding when drawer closed
-            }}
-          >
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar position="absolute" open={open} hassidebar={hasSidebar}>
+        <Toolbar
+          sx={{
+            pr: '24px', // keep right padding when drawer closed
+          }}
+        >
+          {hasSidebar &&
             <IconButton
               edge="start"
               color="inherit"
@@ -72,41 +68,34 @@ const Layout: FunctionComponent = ({ children }) => {
               }}
             >
               <MenuIcon />
-            </IconButton>
-            <Typography
-              component="h1"
-              variant="h6"
-              color="inherit"
-              noWrap
-              sx={{ flexGrow: 1 }}
-            >
-              Dashboard
-            </Typography>
-            <IconButton color="inherit">
-              <Badge badgeContent={4} color="secondary">
-                <NotificationsIcon />
-              </Badge>
-            </IconButton>
-          </Toolbar>
-        </AppBar>
-        <Sidebar open={open} toggleDrawer={toggleDrawer} />
-        <Box
-          component="main"
-          sx={{
+            </IconButton>}
+          <AppLogo />
+        </Toolbar>
+      </AppBar>
+      {hasSidebar && <Sidebar open={open} toggleDrawer={toggleDrawer} />}
+      <Box
+        component="main"
+        sx={{
+          ...(hasSidebar && {
             backgroundColor: (theme) =>
               theme.palette.mode === 'light'
                 ? theme.palette.grey[100]
                 : theme.palette.grey[900],
-            flexGrow: 1,
-            height: '100vh',
-            overflow: 'auto',
-          }}
-        >
-          <Toolbar />
-          {children}
-        </Box>
+          }),
+          ...(!hasSidebar && {
+            backgroundImage: `url(${Image})`,
+            backgroundSize: '100% auto',
+            minHeight: '100%',
+          }),
+          flexGrow: 1,
+          height: '100vh',
+          overflow: 'auto',
+        }}
+      >
+        <Toolbar />
+        {children}
       </Box>
-    </ThemeProvider>
+    </Box >
   );
 };
 
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,18 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import Router from './Router';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 /* Global styles */
 import 'assets/styles/common.scss';
 import { Provider } from 'react-redux';
-import {store} from './redux/store';
+import { store } from './redux/store';
+
+// TODO configure theme
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#152238'
+    },
+  },
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <Router />
-    </Provider>
-  </React.StrictMode>,
+  <ThemeProvider theme={theme}>
+    <React.StrictMode>
+      <Provider store={store}>
+        <Router />
+      </Provider>
+    </React.StrictMode>
+  </ThemeProvider>
+  ,
   document.getElementById('root')
 );
 
