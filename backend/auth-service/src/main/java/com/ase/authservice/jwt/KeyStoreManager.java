335392bed7aa0ee2e57fe1d36401df1c6e6babd3
daviddias99 <up201705373@fe.up.pt>
2022-01-04 16:26:52 +0000

    Cleanup

--- backend/auth-service/src/main/java/com/ase/authservice/jwt/KeyStoreManager.java
+++ backend/auth-service/src/main/java/com/ase/authservice/jwt/KeyStoreManager.java
@@ -5,11 +5,7 @@ import org.springframework.util.ResourceUtils;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.security.Key;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
@@ -23,49 +19,48 @@ public class KeyStoreManager {
 
     private char[] password = "Password1".toCharArray();
 
-    public KeyStoreManager() throws KeyStoreException, IOException {
+    public KeyStoreManager() throws KeyStoreException, IOException{
         loadKeyStore();
     }
 
-    public File loadEmployeesWithSpringInternalClass() throws FileNotFoundException {
-        return ResourceUtils.getFile(
-                "classpath:auth.keystore");
-    }
-
-    public void loadKeyStore() throws KeyStoreException, IOException {
+    public void loadKeyStore() throws KeyStoreException, IOException{
         keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
 
-        // // ClassLoader loader = KeyStoreManager.class.getClassLoader();
-        // URL url1 = getClass().getClassLoader().getResource("bootstrap.yml");
-        // InputStream url2 = getClass().getClassLoader().getResourceAsStream("auth.keystore");
-        File keystoreFile = new File("src/main/resources/auth.keystore");
+        FileInputStream fis = null;
 
-        try (FileInputStream fis = new FileInputStream(keystoreFile)) {
-            keyStore.load(fis, password);
+        try{
+            File keystoreFile = ResourceUtils.getFile("classpath:auth.keystore");
+            fis = new FileInputStream(keystoreFile);
+            keyStore.load(fis,password);
             keyAlias = keyStore.aliases().nextElement();
-
-        } catch (Exception e) {
+        } catch(Exception e){
             System.err.println("Error when loading Keystore");
             e.printStackTrace();
         }
+        finally {
+            if(fis != null){
+                fis.close();
+            }
+        }
     }
 
-    protected PublicKey getPublicKey() {
-        try {
+    protected PublicKey getPublicKey(){
+        try{
             return keyStore.getCertificate(keyAlias).getPublicKey();
 
-        } catch (Exception e) {
+        } catch(Exception e){
             e.printStackTrace();
             return null;
         }
     }
 
-    protected Key getPrivateKey() {
-        try {
+    protected Key getPrivateKey(){
+        try{
             return keyStore.getKey(keyAlias, "Password1".toCharArray());
-        } catch (Exception e) {
+        } catch(Exception e){
             e.printStackTrace();
             return null;
         }
     }
 }
+
