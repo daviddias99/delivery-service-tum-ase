b70e38f3b28559ef8962e3ba273b81fc6ac82e25
daviddias99 <up201705373@fe.up.pt>
2022-01-14 09:47:21 +0100

    Remove tracking from homepage

--- frontend/src/components/custom/homepage/Homepage.tsx
+++ frontend/src/components/custom/homepage/Homepage.tsx
@@ -1,57 +1,24 @@
 import React from 'react';
-import { Paper, TextField, Grid, Button } from '@mui/material';
+import { Paper } from '@mui/material';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Title from 'components/common/Title/Title';
-import { useNavigate } from 'react-router-dom';
-import api from 'services/api';
-import { AxiosResponse } from 'axios';
+import Image from 'assets/images/process.png';
+
 
 const Homepage = () => {
-
-  const [trackingCode, setTrackingCode] = React.useState('');
-  const [errorText, setErrorText] = React.useState('');
-  const navigate = useNavigate();
-
-  const onClick = () => {
-    if (!trackingCode || trackingCode === '') {
-      return;
-    }
-
-    const callback = (response: AxiosResponse<any, any>) => {
-      if (response.status !== 200) {
-        setErrorText('We could not find any delivery associated with the given tracking number.');
-        return;
-      }
-      navigate(`/delivery/${response.data.id}`);
-    };
-
-
-    api.getByTrackingCode(trackingCode, callback);
-  };
-
   return (
     <Container maxWidth="md" sx={{ mt: 6, mb: 4, p: 4 }}>
-      <Paper sx={{ p: '6em', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <Paper sx={{ p: '4em', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
         <Title>Welcome to the ASE Delivery System!</Title>
         <Typography variant="h6">We&apos;re happy you chose us for your delivery needs.</Typography>
         <Typography align="center" >
-          Want to know where your package is? No problem, just insert your tracking code into the box bellow, or alternatively login.
+          Using ASE Delivery is easy, your delivery service professionals will deliver your package to one of our Boxes, where it will be waiting for you!
         </Typography>
-        <Grid container sx={{ mt: 3 }} spacing={1}>
-          <Grid item xs={11}>
-            <TextField helperText={errorText} value={trackingCode} onChange={(event) => setTrackingCode(event.target.value)} sx={{ width: '100%' }} label="Tracking code" id="outlined-basic" variant="outlined" />
-          </Grid>
-
-          <Grid item xs={1} display="flex" alignItems="center" >
-            <Button onClick={onClick}>Go</Button>
-          </Grid>
-        </Grid>
-
+        <img style={{ height: '85px', marginTop: '2em' }} src={Image} />
       </Paper>
     </Container>
   );
-
 };
 
 export default Homepage;
