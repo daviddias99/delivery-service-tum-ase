7a1dc86ffe505fae7355c5d39e63ef8b8036f68e
qirtum <r.qi@tum.de>
2022-02-01 14:46:07 +0100

    added CORS

--- backend/gateway-server/src/main/java/com/ase/AuthConfig.java
+++ backend/gateway-server/src/main/java/com/ase/AuthConfig.java
@@ -2,15 +2,18 @@ package com.ase;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 
-
+@Configuration
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
 public class AuthConfig {
@@ -29,6 +32,26 @@ public class AuthConfig {
         return http.build();
     }
 
+    @Bean
+    public WebFluxConfigurer corsConfigurer(){
+        return new WebFluxConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+
+                registry.addMapping("/box/check")
+                        .allowedOrigins("*");
+
+
+                registry.addMapping("/**")
+                        .allowedOrigins("http://your-frontend-here.com")
+                        .allowedMethods("PUT", "GET", "POST", "DELETE")
+                        .allowedHeaders("header1", "header2", "header3")
+                        .exposedHeaders("header1", "header2")
+                        .allowCredentials(true).maxAge(3600);
+            }
+
+        };
+    }
 
 }
 
